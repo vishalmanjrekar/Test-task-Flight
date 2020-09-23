1dcfb4dea1c1814ef340c6d90e807655eda43886
vishalmanjrekar <manjrekarv48@gmail.com>
2020-09-23 13:07:15 +0530

    minor changes

--- src/component/Checkout/checkout.js
+++ src/component/Checkout/checkout.js
@@ -15,10 +15,11 @@ export class Checkout extends Component {
                 timeDuration += item2.duration + item2.layoverDuration
             }
         })
-        let timeDuration1 = Object.keys(itinerary1).length > 0 &&itinerary1.inFlights && itinerary1.inFlights[0].duration
-        Object.keys(itinerary1).length > 0 && itinerary1.inFlights && itinerary1.inFlights.length > 1 && itinerary.inFlights.map((item2, index1) => {
-            if(index1 !== 0) {
-                timeDuration += item2.duration + item2.layoverDuration
+
+        let timeDuration1 = Object.keys(itinerary1).length > 0 && itinerary1.inFlights[0].duration
+        Object.keys(itinerary1).length > 0 && itinerary1.inFlights.length > 1 && itinerary1.inFlights.map((item3, indexItem) => {
+            if(indexItem !== 0) {
+                timeDuration1 += item3.duration + item3.layoverDuration
             }
         })
         return (
@@ -64,14 +65,14 @@ export class Checkout extends Component {
                                             <div className="dept-time ">{data.leaving}</div>
                                             <p className="font14 bold">{moment(data.departDateTime).format('ddd,DD MMM YY')}</p>
                                             <p className="dept-city">
-                                                <span className="bold">{itinerary.originGeoname.City}</span>{data.originAirportFullName}</p>
+                                                <span className="bold">{dataIndex === 0 && itinerary.originGeoname.City}</span>{data.originAirportFullName}</p>
                                         </div>
                                         <p className="fli-stops">{Math.floor(data.duration/60)}hrs {data.duration%60}mins</p>
                                         <div className="fli-time-section pull-left">
                                             <p className="dept-time mb-0">{data.arriving}</p>
                                             <p className="font14 bold">{moment(data.arrivingDateTime).format('MMM DD YY')}</p>
                                             <span className="arrival-city">
-                                                <span className="bold">{itinerary.destinationGeoname.City}</span>
+                                                {(itinerary.outFlights.length - 1) === dataIndex && <span className="bold">{itinerary.destinationGeoname.City}</span>}
                                                 <p>{data.destinationAirportFullName}</p>
                                             </span></div>
                                     </div>
@@ -95,16 +96,16 @@ export class Checkout extends Component {
                                     <p>{moment(itinerary1.inFlights[0].departDateTime).format('MMM DD YY')}</p>
                                 </div>
                                 <div className="class-section">
-                                    <p><span>{itinerary1.origin} - {itinerary1.destination}</span></p>
-                                    <p>{itinerary1.outStops === 0 ? '' : `${itinerary1.outStops} stop |`} {Math.floor(timeDuration1 / 60)}hrs {timeDuration1 % 60}mins</p>
+                                    <p><span>{itinerary1.destination} - {itinerary1.origin}</span></p>
+                                    <p>{itinerary1.inStops === 0 ? '' : `${itinerary1.inStops} stop |`} {Math.floor(timeDuration1 / 60)}hrs {timeDuration1 % 60}mins</p>
                                 </div>
                             </div>
                             {/*<span className="fare-wrap"><p> Fare Rules</p></span>*/}
                         </div>
-                        {itinerary1.inFlights.map((data, dataIndex) => {
+                        {itinerary1.inFlights.map((data, dataIndex1) => {
 
                             return (
-                                <div className="itnry-flt-body" key={dataIndex}>
+                                <div className="itnry-flt-body" key={dataIndex1}>
                                     {data.layoverDuration > 0 && <div className="flight-hold-duration"><span>Changes of Planes </span><strong>{Math.floor(data.layoverDuration/60)}hrs {data.layoverDuration%60}mins</strong> layOver in <strong>{data.originAirportFullName}</strong></div>}
                                     <div className="flight-next-set">
                                     <div className="airline-info">
@@ -119,8 +120,7 @@ export class Checkout extends Component {
                                             <div className="dept-time ">{data.leaving}</div>
                                             <p className="font14 bold">{moment(data.departDateTime).format('ddd,DD MMM YY')}</p>
                                             <p className="dept-city">
-                                                <span
-                                                    className="bold">{itinerary.originGeoname.City}</span>{data.originAirportFullName}
+                                                <span className="bold">{dataIndex1 === 0 && itinerary1.originGeoname.City}</span>{data.originAirportFullName}
                                             </p>
                                         </div>
                                         <p className="fli-stops">{Math.floor(data.duration / 60)}hrs {data.duration % 60}mins</p>
@@ -128,7 +128,7 @@ export class Checkout extends Component {
                                             <p className="dept-time mb-0">{data.arriving}</p>
                                             <p className="font14 bold">{moment(data.arrivingDateTime).format('MMM DD YY')}</p>
                                             <span className="arrival-city">
-                                                <span className="bold">{itinerary.destinationGeoname.City}</span>
+                                                {(itinerary1.inFlights.length - 1) === dataIndex1 && <span className="bold">{itinerary1.destinationGeoname.City}</span>}
                                                 <p>{data.destinationAirportFullName}</p>
                                             </span>
                                         </div>
